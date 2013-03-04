62079905e905895e4e297bee577463518ff6231f
Connor Ledgerwood <clkv5@mail.umkc.edu>
2013-03-03 18:23:38 -0600

    Updated AccountService methods

--- Source/WebServices/App_Code/AccountService.cs
+++ Source/WebServices/App_Code/AccountService.cs
@@ -13,7 +13,9 @@ using System.Web.Services;
 public class WebService : System.Web.Services.WebService {
 
 
-    /* Global variables */
+    /*************************************************************
+     *  Class Variables
+    *************************************************************/
     public enum ID_TYPE
     {
         ACCOUNT_ID=0,
@@ -39,35 +41,34 @@ public class WebService : System.Web.Services.WebService {
     {
     }
 
+    /*************************************************************
+     *  Service Methods
+    *************************************************************/
 
     [WebMethod]
-    public string Register( string name, string email, string password, string passwordRepeat )
+    public string Register( string name, string email, string password, string passwordRepeat, int type )
     {
         /* Check user doesn't exist in database */
-        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ProjectDatabase"].ConnectionString);
-        conn.Open();
-
-        SqlCommand scanUsers = new SqlCommand("SELECT count(*) FROM Accounts WHERE emailAddress='" + email + "'", conn);
-        int count = (int)scanUsers.ExecuteScalar();
-        scanUsers.Dispose();
-
-        if (count != 0)
-            //return "Error registering:  An account already exists with the given email.";
-            return count.ToString();
+        if( AccountExists( email ) )
+            return "Error registering:  An account already exists with the given email.";
 
         /* Check user credentials */
         if( password != passwordRepeat )
             return "Error registering:  Password fields do not match.";
 
-        /* Add user to database */
+        /* Open connection to database */
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ProjectDatabase"].ConnectionString);
+        conn.Open();
+
+        /* Add new account */
         int id = generateNewID( ID_TYPE.ACCOUNT_ID );
-        //TODO:  handle accountType and linkedID
         SqlCommand writeCmd = new SqlCommand("INSERT INTO Accounts(id, accountType, linkedID, emailAddress, userName, password) " +
-                                              " values('"+ id + "','" + 0 + "','" + id + "','" + email + "','" + name + "','" + password + "')", conn);
+                                              " values('"+ id + "','" + type + "','" + -1 + "','" + email + "','" + name + "','" + password + "')", conn);
         writeCmd.ExecuteNonQuery();
         writeCmd.Dispose();
-
         conn.Close();
+
+        // Note:  This should return additional information to help link accounts as a parent
         return "Registration Successful";
     }
 
@@ -75,34 +76,143 @@ public class WebService : System.Web.Services.WebService {
     [WebMethod]
     public string Login(string email, string password)
     {
+        /* Check user exists */
+        if (  !AccountExists( email )  )
+            return "Error logging in:  Invalid email.";
+
+        /* Check user credentials */
+        string pass = getPassword( email );
+        if ( password != pass )
+            return "Error logging in:  Invalid password.";
+
+        return "Login Successful";
+    }
+
+
+    [WebMethod]
+    public string LinkAccounts( string parentEmail, string studentEmail )
+    {
+
+        /* Check users exist in database */
+        if (   !AccountExists(parentEmail)
+            || !AccountExists(studentEmail)
+           )
+            return "Error:  an account does not exist.";
+
+        /* Check user credentials */
+        int type = getAccountType(parentEmail);
+        if ( (ACCOUNT_TYPE)type != ACCOUNT_TYPE.PARENT )
+            return "Error:  first account must be guardian type";
+        type = getAccountType(studentEmail);
+        if ( (ACCOUNT_TYPE)type != ACCOUNT_TYPE.STUDENT )
+            return "Error:  second account must be student type";
+
+        //TODO:  add security to enforce that links aren't formed to other parents' children
+
         /* Connect to database */
         SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ProjectDatabase"].ConnectionString);
         conn.Open();
 
-        /* Check user exists in database */
+        /* Link student to parent */
+        int linkID = getAccountID(parentEmail);
+        SqlCommand linkStudent = new SqlCommand("UPDATE Accounts SET linkedID='" + linkID + "' WHERE emailAddress='" + studentEmail + "'", conn);
+        linkStudent.ExecuteNonQuery();
+        linkStudent.Dispose();
+
+        /* Linke parent to student */
+        linkID = getAccountID(studentEmail);
+        SqlCommand linkParent = new SqlCommand("UPDATE Accounts SET linkedID='" + linkID + "' WHERE emailAddress='" + parentEmail + "'", conn);
+        linkParent.ExecuteNonQuery();
+        linkParent.Dispose();
+
+        conn.Close();
+
+        return "Accounts linked successfully.";
+    }
+
+
+    /*************************************************************
+     *  Protected Methods
+     *************************************************************/
+
+    protected bool AccountExists(string email)
+    {
+        /* Connect to database */
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ProjectDatabase"].ConnectionString);
+        conn.Open();
+
+        /* Check users exist in database */
         SqlCommand scanUsers = new SqlCommand("SELECT count(*) FROM Accounts WHERE emailAddress='" + email + "'", conn);
         int count = Convert.ToInt32(scanUsers.ExecuteScalar());
         scanUsers.Dispose();
+        conn.Close();
 
         if (count == 0)
-            return "Error logging in:  Account does not exist";
+            return false;
+        return true;
+    }
 
-        /* Acquire user info */
+    protected string getPassword(string email)
+    {
+        /* Connect to database */
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ProjectDatabase"].ConnectionString);
+        conn.Open();
+
+        /* Get account info  */
         SqlCommand getInfo = new SqlCommand("SELECT * FROM Accounts WHERE emailAddress='" + email + "'", conn);
         SqlDataReader reader = getInfo.ExecuteReader();
 
+        /* Read password */
         reader.Read();
         string pass = reader["password"].ToString();
         reader.Close();
-        reader.Dispose(); 
+        reader.Dispose();
         getInfo.Dispose();
-
-        /* Check user credentials */
         conn.Close();
-        if (password != pass)
-            return "Error logging in:  Invalid password";
 
-        return "Login Successful";
+        return pass;
+    }
+
+    protected int getAccountID(string email)
+    {
+        /* Connect to database */
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ProjectDatabase"].ConnectionString);
+        conn.Open();
+
+        /* Get account info  */
+        SqlCommand getInfo = new SqlCommand("SELECT * FROM Accounts WHERE emailAddress='" + email + "'", conn);
+        SqlDataReader reader = getInfo.ExecuteReader();
+
+        /* Read ID */
+        reader.Read();
+        int id = (int)reader["id"];
+        reader.Close();
+        reader.Dispose();
+        getInfo.Dispose();
+        conn.Close();
+
+        return id;
+    }
+
+    protected int getAccountType(string email)
+    {
+        /* Connect to database */
+        SqlConnection conn = new SqlConnection(ConfigurationManager.ConnectionStrings["ProjectDatabase"].ConnectionString);
+        conn.Open();
+
+        /* Get account info  */
+        SqlCommand getInfo = new SqlCommand("SELECT * FROM Accounts WHERE emailAddress='" + email + "'", conn);
+        SqlDataReader reader = getInfo.ExecuteReader();
+
+        /* Read account type */
+        reader.Read();
+        int type = (int)reader["accountType"];
+        reader.Close();
+        reader.Dispose();
+        getInfo.Dispose();
+        conn.Close();
+
+        return type;
     }
 
 
